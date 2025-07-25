b8750aaa9dd1a9fe940726e7fadc39b538a936be
eduardocd18 <javierecd@gmail.com>
2025-07-25 05:32:32 -0400

    668. Creando la vista de Login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,30 @@
-<?php echo $titulo ?>
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+
+    <p class="auth__texto">Inicio sesión en Deb Wev Camp</p>
+
+    <!-- Formulario para inicio de sesión -->
+    <form action="" class="formulario">
+
+        <!-- Ingreso de correo -->
+        <div class="formulario__campo">
+            <label for="email" class="formulario__label">Correo</label>
+            <input type="email" class="formulario__input" placeholder="Ingresa tu corro electrónico" id="email" name="email">
+        </div>
+
+        <!-- Ingreso de contraseña -->
+        <div class="formulario__campo">
+            <label for="password" class="formulario__label">Contraseña</label>
+            <input type="password" class="formulario__input" placeholder="Ingresa tu contraseña" id="password" name="password">
+        </div>
+
+        <!-- Enviar los datos -->
+        <input type="submit" class="formulario__submit" value="Iniciar Sesión">
+    </form>
+
+    <!-- Acciones extras -->
+    <div class="acciones">
+        <a href="/registro" class="acciones__enlace">¿Aún sin cuenta? ¡Crea una!</a> <!-- Crear nueva cuenta -->
+        <a href="/olvide" class="acciones__enlace">¿Olvidaste la clave? ¡Recupérala!</a> <!-- Contraseña olvidada -->
+    </div>
+</main>
