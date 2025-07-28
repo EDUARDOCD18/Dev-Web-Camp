815e7866f89b65c8d07e3ac87310d2115b25b6cd
eduardocd18 <javierecd@gmail.com>
2025-07-28 07:15:47 -0400

    675. Añadiendo el HTML del Header

---
+++ views/templates/header.php
@@ -0,0 +1,27 @@
+<!-- Cabecera -->
+<header class="header"></header>
+<!-- Contenedor de cabecera -->
+<div class="header__contenedor">
+    <!-- Barra de navegación -->
+    <nav class="header__navegacion">
+        <a href="/registro" class="haeder__enlace">Registro</a>
+        <a href="/login" class="haeder__enlace">Iniciar Sesión</a>
+    </nav>
+    <!-- Fin barra de nevegación -->
+
+    <!-- Contenido de cabecera -->
+    <div class="header__contenido">
+        <a href="/">
+            <h1 class="header__logo">&#60;DevWebCamp/></h1>
+        </a>
+
+        <p class="header__texto">Octubre, 4 y 5 - 2025</p>
+        <p class="header__texto header__texto--modalidad">Modalidad: En línea y Presencial</p>
+
+        <a href="/registro" class="header__boton">Comprar pase</a>
+    </div>
+    <!-- Fin contenido de cabecera -->
+</div>
+<!-- Fin contenedor cabecera -->
+</header>
+<!-- Fin cabecera -->
