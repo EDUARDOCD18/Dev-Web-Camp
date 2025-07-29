4c7547b153cde0dfd447423c440b09d1a6f2064b
eduardocd18 <javierecd@gmail.com>
2025-07-29 09:45:43 -0400

    680. Creando una Barra de Navegación

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,27 +1,49 @@
 <!-- Cabecera -->
 <header class="header">
-<!-- Contenedor de cabecera -->
-<div class="header__contenedor">
-    <!-- Barra de navegación -->
-    <nav class="header__navegacion">
-        <a href="/registro" class="header__enlace">Registro</a>
-        <a href="/login" class="header__enlace">Iniciar Sesión</a>
-    </nav>
-    <!-- Fin barra de nevegación -->
+    <!-- Contenedor de cabecera -->
+    <div class="header__contenedor">
+        <!-- Barra de navegación -->
+        <nav class="header__navegacion">
+            <a href="/registro" class="header__enlace">Registro</a>
+            <a href="/login" class="header__enlace">Iniciar Sesión</a>
+        </nav>
+        <!-- Fin barra de nevegación -->
 
-    <!-- Contenido de cabecera -->
-    <div class="header__contenido">
-        <a href="/">
-            <h1 class="header__logo">&#60;DevWebCamp/></h1>
-        </a>
+        <!-- Contenido de cabecera -->
+        <div class="header__contenido">
+            <a href="/">
+                <h1 class="header__logo">&#60;DevWebCamp/></h1>
+            </a>
 
-        <p class="header__texto">Octubre, 4 y 5 - 2025</p>
-        <p class="header__texto header__texto--modalidad">Modalidad: En línea y Presencial</p>
+            <p class="header__texto">Octubre, 4 y 5 - 2025</p>
+            <p class="header__texto header__texto--modalidad">Modalidad: En línea y Presencial</p>
 
-        <a href="/registro" class="header__boton">Comprar pase</a>
+            <a href="/registro" class="header__boton">Comprar pase</a>
+        </div>
+        <!-- Fin contenido de cabecera -->
     </div>
-    <!-- Fin contenido de cabecera -->
-</div>
-<!-- Fin contenedor cabecera -->
+    <!-- Fin contenedor cabecera -->
 </header>
 <!-- Fin cabecera -->
+
+<!-- Barra -->
+<div class="barra">
+    <!-- Barra contenido -->
+    <div class="barra__contenido">
+        <a href="/">
+            <h2 class="barra__logo">&#60;DevWebCamp/></h2>
+        </a>
+
+        <!-- Navegación -->
+        <nav class="navegacion">
+            <a href="/devwebcamp" class="navegacion__enlace">Evento</a>
+            <a href="/paquetes" class="navegacion__enlace">Paquetes</a>
+            <a href="/workshop-conferencias" class="navegacion__enlace">Workshop / Conferencias</a>
+            <a href="/registro" class="navegacion__enlace">Comprar Pase</a>
+            <!-- Fin Navegación -->
+
+        </nav>
+    </div>
+    <!-- Fin Barra contenido -->
+</div>
+<!-- Fin barra -->
