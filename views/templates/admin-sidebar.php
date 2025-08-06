979e1201a16fcc14dcf9a6733dd8922a8acc40b2
eduardocd18 <javierecd@gmail.com>
2025-08-06 05:56:47 -0400

    692. CSS al Panel de Administración

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -3,27 +3,27 @@
 <aside class="dashboard__sidebar">
     <nav class="dashboard__menu">
         <a href="/admin/dashboard" class="dashboard__enlace">
-            <i class="fa-solid fa-house-chimney"></i>
+            <i class="fa-solid fa-house-chimney dashboard__icono"></i>
             <span class="dashboard__menu-texto">Inicio</span>
         </a>
 
         <a href="/admin/ponentes" class="dashboard__enlace">
-            <i class="fa-solid fa-microphone-lines"></i>
+            <i class="fa-solid fa-microphone-lines dashboard__icono"></i>
             <span class="dashboard__menu-texto">Ponentes</span>
         </a>
 
         <a href="/admin/eventos" class="dashboard__enlace">
-            <i class="fa-solid fa-calendar-days"></i>
+            <i class="fa-solid fa-calendar-days dashboard__icono"></i>
             <span class="dashboard__menu-texto">Eventos</span>
         </a>
 
         <a href="/admin/registrados" class="dashboard__enlace">
-            <i class="fa-solid fa-users"></i>
+            <i class="fa-solid fa-users dashboard__icono"></i>
             <span class="dashboard__menu-texto">Registrados</span>
         </a>
-        
+
         <a href="/admin/registrados" class="dashboard__enlace">
-            <i class="fa-solid fa-gifts"></i>
+            <i class="fa-solid fa-gifts dashboard__icono"></i>
             <span class="dashboard__menu-texto">Regalos</span>
         </a>
     </nav>
