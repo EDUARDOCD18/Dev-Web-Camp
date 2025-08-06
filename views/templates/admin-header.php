1a2cd5b7266d93e99aada32a27084898653dcdf9
eduardocd18 <javierecd@gmail.com>
2025-08-06 05:21:30 -0400

    690. Primeros Pasos creando el Dashboard

---
+++ views/templates/admin-header.php
@@ -0,0 +1,16 @@
+<!-- ESTILOS PARA EL HEADER DEL  PANEL DE ADMINISTRACIÓN -->
+
+<header class="dashboard__header">
+    <div class="dashboard__header-grid">
+        <a href="/">
+            <h2 class="dashboard__logo">&#60;DevWebCamp></h2>
+        </a>
+
+        <!-- Navegación -->
+        <nav class="dashboard__nav">
+            <form action="/logout" method="POST" class="dashboard__form">
+                <input type="submit" value="Cerrar Sesión" class="dashboard__submit--logout">
+            </form> <!-- .dashboard__form -->
+        </nav> <!-- .dashboard__nav -->
+    </div> <!-- .dashboard__header-grid -->
+</header> <!-- .dashboard__header -->
