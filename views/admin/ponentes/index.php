f86d4cf45158685ccd60b4ba41f51fce270d1ce0
eduardocd18 <javierecd@gmail.com>
2025-08-07 05:37:49 -0400

    695. Routing en Ponentes y Primeros Pasos

--- views/admin/ponentes/index.php
+++ views/admin/ponentes/index.php
@@ -1,2 +1,9 @@
 <h2 class="dashboard__heading"><?php echo $titulo; ?>
 </h2>
+
+<div class="dashboard__contenedor-boton">
+    <a href="/admin/ponentes/crear" class="dashboard__boton">
+        <i class="fa-solid fa-circle-plus"></i>
+        Añadir ponente
+    </a>
+</div>
