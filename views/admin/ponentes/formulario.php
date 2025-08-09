c077660a0ea052ef2b558522947b930421b1fbf6
eduardocd18 <javierecd@gmail.com>
2025-08-09 05:45:19 -0400

    698. Añadiendo Campos Extras - Tags y Fotografía

--- views/admin/ponentes/formulario.php
+++ views/admin/ponentes/formulario.php
@@ -3,13 +3,51 @@
 
     <!-- Nombre del ponente -->
     <div class="formulario__campo">
-        <label for="nombre" class="formulario__label">Nombre:</label>
-        <input type="text" class="formulario__input" id="nombre" placeholder="Nombre del ponente" name="nombre" value="<?php echo $ponente->nombre ?? ''; ?>" 
-    </div>
+        <label for="nombre" class="formulario__label">Ricardo:</label>
+        <input type="text" class="formulario__input" id="nombre" placeholder="Nombre del ponente" name="nombre" value="<?php echo $ponente->nombre ?? ''; ?>" </div>
 
-    <!-- Apellido del ponente -->
-    <div class="formulario__campo">
-        <label for="apellido" class="formulario__label">Apellido:</label>
-        <input type="text" class="formulario__input" id="apellido" placeholder="Apellido del ponente" name="apellido" value="<?php echo $ponente->apellido ?? ''; ?>" 
+        <!-- Apellido del ponente -->
+        <div class="formulario__campo">
+            <label for="apellido" class="formulario__label">Apellido:</label>
+            <input type="text" class="formulario__input" id="apellido" placeholder="Apellido del ponente" name="apellido" value="<?php echo $ponente->apellido ?? ''; ?>" </div>
+        </div>
+
+        <!-- Ciudad del ponente -->
+        <div class="formulario__campo">
+            <label for="ciudad" class="formulario__label">Ciudad:</label>
+            <input type="text" class="formulario__input" id="ciudad" placeholder="Los Ángeles" name="ciudad" value="<?php echo $ponente->ciudad ?? ''; ?>" </div>
+        </div>
+
+        <!-- País del ponente -->
+        <div class="formulario__campo">
+            <label for="pais" class="formulario__label">País:</label>
+            <input type="text" class="formulario__input" id="pais" placeholder="Venezuela" name="pais" value="<?php echo $ponente->pais ?? ''; ?>" </div>
+        </div>
+
+        <!-- Imagen del ponente -->
+        <div class="formulario__campo">
+            <label for="imagen" class="formulario__label">Imagen:</label>
+            <input type="file" class="formulario__input formulario__input--file" id="imagen" name="imagen"" 
+    
     </div>
+</fieldset>
+
+
+<fieldset class=" formulario__fieldset">
+
+            <legend class="formulario__legend">Información Extra</legend>
+
+            <div class="formulario__campo">
+
+                <!-- Áreas de experiencia -->
+                <div class="formulario__campo">
+                    <label for="tags_input" class="formulario__label">Áreas de experiencia (separadas por coma):</label>
+                    <input type="text" class="formulario__input" id="tags_input" placeholder="Ej. Node, PHP, CSS, Laraver, UX / UI">
+                </div>
+
+                <div class="formulario__listado" id="tags">
+
+                </div>
+                <input name="tags" type="hidden" value="<?php echo $ponente->tags ?? ''; ?>">
+            </div>
 </fieldset>
