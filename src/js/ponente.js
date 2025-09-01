4e7714395d3dece4d97ca20b3d305792ec520d58
eduardocd18 <javierecd@gmail.com>
2025-09-01 04:44:54 -0400

    741. Filtrar Ponentes por Búsqueda

--- src/js/ponente.js
+++ src/js/ponente.js
@@ -7,6 +7,8 @@
 
     obtenerPonentes();
 
+    ponentesInput.addEventListener("input", buscarPonentes);
+
     async function obtenerPonentes() {
       const url = `/api/ponentes`;
       const respuesta = await fetch(url);
@@ -24,7 +26,21 @@
         };
       });
 
-      console.log(ponentes);
+      // console.log(ponentes);
+    }
+
+    function buscarPonentes(e) {
+      const busqueda = e.target.value;
+
+      if (busqueda.length > 3) {
+        const expresion = new RegExp(busqueda, "i");
+        ponentesFiltrados = ponentes.filter((ponente) => {
+          if (ponente.nombre.toLowerCase().search(expresion) != -1) {
+            return ponente;
+          }
+        });
+        console.log(ponentesFiltrados);
+      }
     }
   }
 })();
