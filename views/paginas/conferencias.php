9c8c5e2e2f8beedfaca434bd43a715db76651d20
eduardocd18 <javierecd@gmail.com>
2025-09-10 09:17:56 -0400

    754. Creando la página de Workshops y Conferencias y primeros pasos con CSS

---
+++ views/paginas/conferencias.php
@@ -0,0 +1,37 @@
+<main class="agenda">
+    <h2 class="agenda__heading">Workshops & conferencias</h2>
+    <p class="agenda__descripcion">Talleres y Conferencias dictados por expertos en el Desarrollo Web.</p>
+
+    <!-- CONFERENCIAS -->
+    <div class="eventos">
+        <h3 class="eventos__heading">&lt;Conferencias/></h3>
+        <p class="eventos__fecha">Viernes, 3 de octubre</p>
+
+        <div class="eventos__listado">
+            <!-- Los datos de la base de datos van acá -->
+        </div>
+
+        <p class="eventos__fecha">Sábado, 4 de octubre</p>
+
+        <div class="eventos__listado">
+            <!-- Los datos de la base de datos van acá -->
+        </div>
+    </div>
+
+    <!-- WORKSHOPS -->
+    <div class="eventos eventos--workshops">
+        <h3 class="eventos__heading">&lt;Workshops/></h3>
+        <p class="eventos__fecha">Viernes, 3 de octubre</p>
+
+        <div class="eventos__listado">
+            <!-- Los datos de la base de datos van acá -->
+        </div>
+
+        <p class="eventos__fecha">Sábado, 4 de octubre</p>
+
+        <div class="eventos__listado">
+            <!-- Los datos de la base de datos van acá -->
+        </div>
+    </div>
+
+</main>
