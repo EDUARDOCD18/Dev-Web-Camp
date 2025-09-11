e3a8f6287b7a5baa786fe9098fc1c9ad30c19e05
eduardocd18 <javierecd@gmail.com>
2025-09-11 09:58:38 -0400

    757. Aplicando CSS a los eventos

--- views/paginas/conferencias.php
+++ views/paginas/conferencias.php
@@ -20,9 +20,7 @@
                         <h4 class="evento__nombre"><?php echo $evento->nombre; ?></h4>
 
                         <!-- Descripción del evento -->
-                        <div>
-                            <p class="evento__descripcion"><?php echo $evento->descripcion ?></p>
-                        </div>
+                        <p class="evento__introduccion"><?php echo $evento->descripcion ?></p>
 
                         <!-- Autor del evento -->
                         <div class="evento__autor-info">
@@ -30,7 +28,7 @@
                             <picture>
                                 <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen; ?>.webp" type="image/webp">
                                 <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen; ?>.png" type="image/png">
-                                <img src="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen; ?>.png" alt="imagen ponenete">
+                                <img class="evento__imagen-autor" loading="lazy" width="200" height="300" src="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen; ?>.png" alt="imagen ponenete">
                             </picture>
 
                             <p class="evento__autor-nombre"><?php echo $evento->ponente->nombre . " " . $evento->ponente->apellido ?></p>
