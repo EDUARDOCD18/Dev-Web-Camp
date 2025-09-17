57ca4ce9265922a6438a72f2a4d26eafc8c2a03a
eduardocd18 <javierecd@gmail.com>
2025-09-16 20:08:35 -0400

    760. Crear el Carousel con Swiper

--- views/paginas/conferencias.php
+++ views/paginas/conferencias.php
@@ -1,68 +1,58 @@
 <main class="agenda">
-    <h2 class="agenda__heading">Workshops & conferencias</h2>
-    <p class="agenda__descripcion">Talleres y Conferencias dictados por expertos en el Desarrollo Web.</p>
+    <h2 class="agenda__heading">Workshops & Conferencias</h2>
+    <p class="agenda__descripcion">Talleres y Conferencias dictados por expertos en desarrollo web</p>
 
-    <!-- CONFERENCIAS -->
     <div class="eventos">
-        <h3 class="eventos__heading">&lt;Conferencias/></h3>
-        <p class="eventos__fecha">Viernes, 3 de octubre</p>
+        <h3 class="eventos__heading">&lt;Conferencias /></h3>
+        <p class="eventos__fecha">Viernes 5 de Octubre</p>
 
         <div class="eventos__listado slider swiper">
             <div class="swiper-wrapper">
-                <?php foreach ($eventos['conferencias_v'] as $evento) { ?>
-                    <div class="evento swiper-slider">
-                        <!-- Hora del evento -->
-                        <div class="evento__hora"><?php echo $evento->hora->hora ?></div>
-
-                        <!-- Nombre del evento -->
-                        <div class="evento__informacion">
-                            <h4 class="evento__nombre"><?php echo $evento->nombre; ?></h4>
-
-                            <!-- Descripción del evento -->
-                            <div>
-                                <p class="evento__introduccion"><?php echo $evento->descripcion ?></p>
-                            </div>
-
-                            <!-- Autor del evento -->
-                            <div class="evento__autor-info">
-
-                                <picture>
-                                    <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen; ?>.webp" type="image/webp">
-                                    <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen; ?>.png" type="image/png">
-                                    <img class="evento__imagen-autor" loading="lazy" width="200" height="300" src="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen; ?>.png" alt="imagen ponenete">
-                                </picture>
-
-                                <p class="evento__autor-nombre"><?php echo $evento->ponente->nombre . " " . $evento->ponente->apellido ?></p>
-                            </div>
-                        </div>
-                    </div>
+                <?php foreach($eventos['conferencias_v'] as $evento ) { ?>
+                    <?php include __DIR__ . '../../templates/evento.php'; ?>
                 <?php } ?>
             </div>
-
             <div class="swiper-button-next"></div>
             <div class="swiper-button-prev"></div>
         </div>
-        <p class="eventos__fecha">Sábado, 4 de octubre</p>
 
-        <div class="eventos__listado">
-            <!-- Los datos de la base de datos van acá -->
+        <p class="eventos__fecha">Sábado 6 de Octubre</p>
+
+        <div class="eventos__listado slider swiper">
+            <div class="swiper-wrapper">
+                <?php foreach($eventos['conferencias_s'] as $evento ) { ?>
+                    <?php include __DIR__ . '../../templates/evento.php'; ?>
+                <?php } ?>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
         </div>
     </div>
 
-    <!-- WORKSHOPS -->
     <div class="eventos eventos--workshops">
-        <h3 class="eventos__heading">&lt;Workshops/></h3>
-        <p class="eventos__fecha">Viernes, 3 de octubre</p>
+        <h3 class="eventos__heading">&lt;Workshops /></h3>
+        <p class="eventos__fecha">Viernes 5 de Octubre</p>
 
-        <div class="eventos__listado">
-            <!-- Los datos de la base de datos van acá -->
+        <div class="eventos__listado slider swiper">
+            <div class="swiper-wrapper">
+                <?php foreach($eventos['workshops_v'] as $evento ) { ?>
+                    <?php include __DIR__ . '../../templates/evento.php'; ?>
+                <?php } ?>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
         </div>
 
-        <p class="eventos__fecha">Sábado, 4 de octubre</p>
+        <p class="eventos__fecha">Sábado 6 de Octubre</p>
 
-        <div class="eventos__listado">
-            <!-- Los datos de la base de datos van acá -->
+        <div class="eventos__listado slider swiper">
+            <div class="swiper-wrapper">
+                <?php foreach($eventos['workshops_s'] as $evento ) { ?>
+                    <?php include __DIR__ . '../../templates/evento.php'; ?>
+                <?php } ?>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
         </div>
     </div>
-
 </main>
