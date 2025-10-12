576233bd31ad9b4c15f2d34d59da5d490e620a39
eduardocd18 <javierecd@gmail.com>
2025-10-11 21:52:27 -0400

    792. Creando Bloques de más y menos lugares disponibles

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Model\Evento;
 use Model\Registro;
 use Model\Usuario;
 use MVC\Router;
@@ -13,7 +14,7 @@ class DashboardController
     {
 
         // Obtener los últimos registros
-        $registros = Registro::get(10);
+        $registros = Registro::get(5);
         foreach ($registros as $registro) {
             $registro->usuario = Usuario::find($registro->usuario_id);
         }
@@ -23,10 +24,18 @@ class DashboardController
         $presenciales = Registro::total('paquete_id', 1);
         $ingresos = ($virtuales * 46.41) + ($presenciales * 189.54);
 
+        // Obtener eventos con más y memos lugares disponibles
+        $menos_lugares = Evento::ordenarLimite('disponibles', 'ASC', 5);
+        $mas_lugares = Evento::ordenarLimite('disponibles', 'DESC', 5);
+        # debuguear($menos_lugares);
+
         $router->render('admin/dashboard/index', [
             'titulo' => 'Dashboard administración',
             'registros' => $registros,
-            'ingresos' => $ingresos
+            'ingresos' => $ingresos,
+            'menos_lugares' => $menos_lugares,
+            'mas_lugares' => $mas_lugares
+
         ]);
     }
 }
--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -10,7 +10,7 @@
             <?php foreach ($registros as $registro) { ?>
                 <div class="bloque__contenido">
                     <p class="bloque__texto"> <?php echo $registro->usuario->nombre . " " . $registro->usuario->apellido; ?>
-                    
+
                     </p>
                 </div>
             <?php } ?>
@@ -19,6 +19,28 @@
         <div class="bloque">
             <h3 class="bloque__heading">Ingresos</h3>
             <p class="bloque__texto--cantidad"><?php echo "$" . $ingresos; ?></p>
+        </div>
+
+        <div class="bloque">
+            <h3 class="bloque__heading">Eventos con menos disponibilidad</h3>
+            <?php foreach ($menos_lugares as $evento) { ?>
+                <div class="bloque__contenido">
+                    <p class="bloque__texto"> <?php echo $evento->nombre . " - " . $evento->disponibles . " Disponibles"; ?>
+
+                    </p>
+                </div>
+            <?php } ?>
+        </div>
+
+        <div class="bloque">
+            <h3 class="bloque__heading">Eventos con más disponibilidad</h3>
+            <?php foreach ($mas_lugares as $evento) { ?>
+                <div class="bloque__contenido">
+                    <p class="bloque__texto"> <?php echo $evento->nombre . " - " . $evento->disponibles . " Disponibles"; ?>
+
+                    </p>
+                </div>
+            <?php } ?>
         </div>
     </div>
 </main>
