5807eb37861d6be5860b5779a1e80983d22b15e0
eduardocd18 <javierecd@gmail.com>
2025-10-11 21:23:48 -0400

    790. Creando un Bloque de últimos registros

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace Controllers;
 
+use Model\Registro;
+use Model\Usuario;
 use MVC\Router;
 
 class DashboardController
@@ -9,8 +11,16 @@ class DashboardController
 
     public static function index(Router $router)
     {
+
+        // Obtener los últimos registros
+        $registros = Registro::get(10);
+        foreach ($registros as $registro) {
+            $registro->usuario = Usuario::find($registro->usuario_id);
+        }
+
         $router->render('admin/dashboard/index', [
             'titulo' => 'Dashboard administración',
+            'registros' => $registros,
         ]);
     }
 }
--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -1,2 +1,18 @@
 <h2 class="dashboard__heading"><?php echo $titulo; ?>
 </h2>
+
+<main class="bloques">
+    <div class="bloques__grid">
+        <div class="bloque">
+            <h3 class="bloque__heading">Últimos registros</h3>
+
+            <!-- Iterar en los registros -->
+            <?php foreach ($registros as $registro) { ?>
+                <div class="bloque__contenido">
+                    <p class="bloque__texto"> <?php echo $registro->usuario->nombre . " " . $registro->usuario->apellido; ?>
+                    </p>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</main>
