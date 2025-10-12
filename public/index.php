bb3f178dc9edd8bf52e80efa5f2ed4fdd33d3143
eduardocd18 <javierecd@gmail.com>
2025-10-11 22:53:04 -0400

    793. Creando una gráfica de regalos y fin de Proyecto

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ use Controllers\APIPonentes;
 use Controllers\PaginasController;
 use MVC\Router;
 use Controllers\APIEventos;
+use Controllers\APIRegalos;
 use Controllers\AuthController;
 use Controllers\DashboardController;
 use Controllers\EventosController;
@@ -66,6 +67,7 @@ $router->post('/admin/eventos/crear', [EventosController::class, 'crear']);
 $router->get('/api/eventos-horario', [APIEventos::class, 'index']);
 $router->get('/api/ponentes', [APIPonentes::class, 'index']);
 $router->get('/api/ponente', [APIPonentes::class, 'ponente']);
+$router->get('/api/regalos', [APIRegalos::class, 'index']);
 
 # Editar un evento
 $router->get('/admin/eventos/editar', [EventosController::class, 'editar']);
--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,5 @@ import "./ponente.js";
 import "./mapa.js";
 import "./tags.js";
 import "./slider.js";
+import "./regalos.js";
 import "./registro.js";
